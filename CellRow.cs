6dd62fcbc7d0f37c3926dd677ebc1f7516d4452a
priyaamies <priya.ssneee@gmail.com>
2015-07-11 00:12:30 +0800

    CellRow CLass

---
+++ CellRow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MineSweeperLogic
+{
+    public class CellRow
+    {
+        public IList<CellRow> Rows { get; set; }
+
+        public IList<Cell> Cells { get; set; }
+
+        public int RowIndex { get { return Rows.IndexOf(this); } }
+ 
+    }
+}
