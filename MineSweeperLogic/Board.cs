1f9731771611273c83d3cc4fa6c6a5e1fc68eab3
priyaamies <priya.ssneee@gmail.com>
2015-07-11 19:42:54 +0800

    Solution

---
+++ MineSweeperLogic/Board.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MineSweeperLogic
+{
+    public static class Board
+    {
+
+        public static IEnumerable<Cell> CalculateFlippedCell(Cell cell)
+        {
+            var result = new List<Cell>();
+            cell.FlagType = FlagType.Flip;
+            //If any adjacent is a Mine, mark as flipped
+            foreach (var b in cell.AllAdjacentCells.Where(x => x.IsCountable))
+            {
+                b.FlagType = FlagType.Flip;
+            }
+            result.Add(cell);
+            //If any adjacent cell is not a Mine, recurse to open other adjacent cells
+            foreach (var b in cell.AllAdjacentCells.Where(x => x.IsFlippable))
+            {
+                result.AddRange(CalculateFlippedCell(b));
+            }
+            return result;
+        }
+
+        public static IList<CellRow> CreateBoard(int size, float mineRate)
+        {
+            var total = size * size;
+            var mineNum = total * mineRate;
+            var random = new Random();
+            var mineIndixe = new List<int>();
+            // Create blank cells, and adds the location of mines
+            for (var i = 0; i < mineNum; i++)
+            {
+                while (true)
+                {
+                    var index = random.Next(total + 1);
+                    if (!mineIndixe.Contains(index))
+                    {
+                        mineIndixe.Add(index);
+                        break;
+                    }
+                }
+            }
+            // Update the cells with mines and blanks
+            var cells = new List<Cell>();
+            for (var i = 0; i < total; i++)
+            {
+                if (mineIndixe.Contains(i))
+                {
+                    cells.Add(new Cell(CellType.Mine));
+                }
+                else
+                {
+                    cells.Add(new Cell(CellType.None));
+                }
+            }
+            //Manipulate Cell into CellRow
+            var rows = new List<CellRow>();
+            for (var i = 0; i < size; i++)
+            {
+                var rowCells = cells.Skip(i * size).Take(size);
+                var cellRow = new CellRow { Cells = rowCells.ToList(), };
+                foreach (var b in rowCells)
+                {
+                    b.Row = cellRow;
+                }
+                rows.Add(cellRow);
+                cellRow.Rows = rows;
+            }
+
+            return rows;
+        }
+
+    }
+
+}
---
+++ MineSweeperUnitTests/MineSweeperTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineSweeperLogic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MineSweeperUnitTests
+{
+    [TestClass]
+    public class MineSweeperTests
+    {
+        [TestMethod]
+        public void CreateBoardTest()
+        {
+            IEnumerable<CellRow> actualCellRows = InitializeBoard();
+            Assert.IsTrue(actualCellRows.First().Cells[1].AllCells.Count() == 9, "Number of Cells matched");
+            Assert.IsTrue(actualCellRows.Count() == 3, "Number of Cell Rows matched");
+            Assert.IsTrue(actualCellRows.Where(x => x.Cells.Any(i => i.CellType == CellType.Mine)).Count() == 1, "There was one mine found");
+        }
+
+        [TestMethod]
+        public void ChangeFlagTypeTest()
+        {
+            Cell cell = new Cell();
+            var flagType = PassDifferentFlagType(cell,FlagType.None);
+            Assert.IsTrue(flagType == FlagType.Mine, "Flag Type is changed when None is Flagged");
+
+            flagType = PassDifferentFlagType(cell, FlagType.Mine);
+            Assert.IsTrue(flagType == FlagType.Flag, "Flag Type is changed when a Mine is flagged?");
+
+            flagType = PassDifferentFlagType(cell, FlagType.Flag);
+            Assert.IsTrue(flagType == FlagType.None, "Flag Type is changed when Flagged?");
+
+        }
+
+        [TestMethod()]        
+        public void CalculateFlippedCellTest()
+        {
+            Cell flipCell = null;
+            IEnumerable<CellRow> actualCellRows = InitializeBoard();   
+         
+            var allCells = actualCellRows.First().Rows[1].Cells[1].AllCells;
+            //Verify that there is one mine
+            Assert.IsTrue(allCells.Any(i => i.CellType == CellType.Mine), "There is one mine in the board");
+            
+            //Pick the center cell to pass for calculation
+            foreach (var cell in allCells)
+            {
+                if (cell.ColumnIndex==1 && cell.RowIndex==1)
+                {
+                    cell.CellType = CellType.None;
+                    flipCell = cell;                    
+                    break;
+                }
+            }          
+            
+            var resultblock = Board.CalculateFlippedCell(flipCell);   
+        
+            //Verify that atleast there are minecount with value 1 adjacent to current block
+            Assert.IsTrue(resultblock.Count(i => i.AllAdjacentCells.Any(j => j.MineCount==1))>1,"There is one mine surrounded");
+            
+        }
+
+        #region HelperMethods
+
+        private static IEnumerable<CellRow> InitializeBoard()
+        {
+            int size = 3;
+            float mineRate = 0.05F;
+            IEnumerable<CellRow> actualCellRows;
+            actualCellRows = Board.CreateBoard(size, mineRate);
+            return actualCellRows;
+
+        #endregion
+        }
+
+        private static FlagType PassDifferentFlagType(Cell cell, FlagType flagType)
+        {
+            cell.FlagType = flagType;
+            var flagTypeValue = Cell.ChangeFlagType(cell);
+            return flagTypeValue;
+        }
+    }
+}
