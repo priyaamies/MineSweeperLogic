50689e2268ddefd6ff55d5bc5b67505c27acd800
priyaamies <priya.ssneee@gmail.com>
2015-07-11 15:26:25 +0800

    Update Board.cs

--- Board.cs
+++ Board.cs
@@ -8,7 +8,7 @@ namespace MineSweeperLogic
 {
     public class Board
     {
-	    public static IList<CellRow> CreateBoard(int size, float mineRate)
+	public static IList<CellRow> CreateBoard(int size, float mineRate)
         {
             var total = size * size;
             var mineNum = total * mineRate;
@@ -27,4 +27,34 @@ namespace MineSweeperLogic
                     }
                 }
             }
+            // Update the cells with mines and blanks
+            var cells = new List<Cell>();
+            for (var i = 0; i < total; i++)
+            {
+                if (mineIndixe.Contains(i))
+                {
+                    cells.Add(new Cell(CellType.Mine));
+                }
+                else
+                {
+                    cells.Add(new Cell(CellType.None));
+                }
+            }
+            //Manipulate Cell into CellRow
+            var rows = new List<CellRow>();
+            for (var i = 0; i < size; i++)
+            {
+                var rowCells = cells.Skip(i * size).Take(size);
+                var cellRow = new CellRow { Cells = rowCells.ToList(), };
+                foreach (var b in rowCells)
+                {
+                    b.Row = cellRow;
+                }
+                rows.Add(cellRow);
+                cellRow.Rows = rows;
+            }
+
+            return rows;
         }
+    }
+}
